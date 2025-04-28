8bdf190022e39a63adbc029a3205c0bdd48a7b75
zulkou <73563319+zulkou@users.noreply.github.com>
2025-04-28 19:29:54 +0700

    feat: add db migrations

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,46 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+# Car Model
+class Car(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    base_price = models.BigIntegerField()
+
+    def __str__(self):
+        return f"{self.brand} {self.model} ({self.year})"
+    
+class Service(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid3,
+        editable=False
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name="services")
+    description = models.TextField()
+    cost = models.BigIntegerField()
+    
+    def __str__(self):
+        return f"{self.car} serviced in {self.created_at}"
+
+class Payment(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name="payments")
+    is_loan = models.BooleanField()
+    annual_interest_rate = models.DecimalField(max_digits=6, decimal_places=2, null=True)
