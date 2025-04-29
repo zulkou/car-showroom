6dc5d85916a0178a7e96e9cb65c5f10645442aec
zulkou <73563319+zulkou@users.noreply.github.com>
2025-04-29 12:28:16 +0700

    feat: add universal template and styling

--- cars/urls.py
+++ cars/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create_car, name='create_car'),
     path('<uuid:car_id>/', views.detail_car, name='detail_car'),
-    path('services/create/<uuid:car_id>', views.create_car_service, name='create_car_service'),
-    path('transactions/create/<uuid:car_id>', views.create_car_transaction, name='create_car_transaction'),
-    path('transaction/confirm/<uuid:car_id>', views.confirm_car_transaction, name='confirm_car_transaction'),
+    path('services/create/<uuid:car_id>/', views.create_car_service, name='create_car_service'),
+    path('transactions/create/<uuid:car_id>/', views.create_car_transaction, name='create_car_transaction'),
+    path('transaction/confirm/<uuid:car_id>/', views.confirm_car_transaction, name='confirm_car_transaction'),
     path('delete/<uuid:car_id>/', views.delete_car, name='delete_car'),
 ]
